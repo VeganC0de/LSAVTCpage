0a8a36c055695a08cb9073d0b9a76b38db8d817b
VeganC0de <danibobadilla00@gmail.com>
2023-09-04 00:08:00 -0300

    Subiendo avances

---
+++ index.js
@@ -0,0 +1,183 @@
+const siteNav = document.querySelector('.js-site-nav')
+const menu = document.querySelector('.js-menu')
+const menuButton = document.querySelector('.js-menu-button')
+const navCurtain = document.querySelector('.js-nav-curtain')
+
+function mobileNavToggler() {
+  const state = {
+    isOpen: false,
+  }
+
+  function showMenu() {
+    siteNav.classList.add('site-nav--is-open')
+    menu.classList.remove('fade-out')
+    menu.classList.add('fade-in')
+  }
+
+  function hideMenu() {
+    siteNav.classList.remove('site-nav--is-open')
+    menu.classList.remove('fade-in')
+    menu.classList.add('fade-out')
+  }
+
+  function curtainUp() {
+    navCurtain.classList.remove('curtain-down')
+    navCurtain.classList.add('curtain-up')
+  }
+
+  function curtainDown() {
+    navCurtain.classList.remove('curtain-up')
+    navCurtain.classList.add('curtain-down')
+  }
+
+  function unfocusButton(event) {
+    menuButton.classList.remove('menu-button__lines--open')
+    menuButton.setAttribute('aria-expanded', 'false')
+  }
+
+  function focusButton(event) {
+    menuButton.classList.add('menu-button__lines--open')
+    menuButton.setAttribute('aria-expanded', 'true')
+  }
+
+  function handleMenuButtonClick() {
+    if (state.isOpen) {
+      hideMenu()
+      unfocusButton()
+      curtainDown()
+      state.isOpen = false
+      return
+    }
+
+    focusButton()
+    curtainUp()
+    state.isOpen = true
+  }
+
+  function handleCurtainAnimationEnd() {
+    if (state.isOpen) {
+      showMenu()
+    }
+  }
+
+  return {
+    handleEvent(event) {
+      if (event.type === 'click') {
+        handleMenuButtonClick()
+        return
+      }
+
+      if (event.type === 'animationend') {
+        handleCurtainAnimationEnd()
+      }
+    },
+
+    init() {
+      menuButton.addEventListener('click', this)
+      navCurtain.addEventListener('animationend', this)
+    },
+  }
+}
+
+mobileNavToggler().init()
+
+
+// LOGO MOVING
+
+let el = document.getElementById('tilt')
+
+/* Get the height and width of the element */
+const height = el.clientHeight
+const width = el.clientWidth
+
+/*
+  * Add a listener for mousemove event
+  * Which will trigger function 'handleMove'
+  * On mousemove
+  */
+el.addEventListener('mousemove', handleMove)
+
+/* Define function a */
+function handleMove(e) {
+  /*
+    * Get position of mouse cursor
+    * With respect to the element
+    * On mouseover
+    */
+  /* Store the x position */
+  const xVal = e.layerX
+  /* Store the y position */
+  const yVal = e.layerY
+  
+  /*
+    * Calculate rotation valuee along the Y-axis
+    * Here the multiplier 20 is to
+    * Control the rotation
+    * You can change the value and see the results
+    */
+  const yRotation = 20 * ((xVal - width / 2) / width)
+  
+  /* Calculate the rotation along the X-axis */
+  const xRotation = -20 * ((yVal - height / 2) / height)
+  
+  /* Generate string for CSS transform property */
+  const string = 'perspective(500px) scale(1.1) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+  
+  /* Apply the calculated transformation */
+  el.style.transform = string
+}
+
+/* Add listener for mouseout event, remove the rotation */
+el.addEventListener('mouseout', function() {
+  el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mousedown event, to simulate click */
+el.addEventListener('mousedown', function() {
+  el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+})
+
+/* Add listener for mouseup, simulate release of mouse click */
+el.addEventListener('mouseup', function() {
+  el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+})
+
+
+
+
+
+//numbers
+
+$.fn.jQuerySimpleCounter = function( options ) {
+  var settings = $.extend({
+    start:  0,
+    end:    100,
+    easing: 'swing',
+    complete: ''
+  }, options );
+
+  var thisElement = $(this);
+
+  $({count: settings.start}).animate({count: settings.end}, {
+    duration: settings.duration,
+    easing: settings.easing,
+    step: function() {
+      var mathCount = Math.ceil(this.count);
+      thisElement.text(mathCount);
+    },
+    complete: settings.complete
+  });
+};
+
+$('#number1').jQuerySimpleCounter({end: 583,duration: 3000});
+$('#number2').jQuerySimpleCounter({end: 946,duration: 3700});
+$('#number3').jQuerySimpleCounter({end: 81,duration: 3100});
+$('#number4').jQuerySimpleCounter({end: 16,duration: 3450});
+$('#number5').jQuerySimpleCounter({end: 472,duration: 3300});
+$('#number6').jQuerySimpleCounter({end: 224,duration: 2175});
+
+
+// Partners slider
+
+
+
