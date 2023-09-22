50338adcaafc2792e61a5629419de2151a005a22
VeganC0de <danibobadilla00@gmail.com>
2023-09-22 20:39:39 -0300

    Agregado de Nosotros y caracteristicas breves

--- index.js
+++ index.js
@@ -177,7 +177,37 @@ $('#number5').jQuerySimpleCounter({end: 472,duration: 3300});
 $('#number6').jQuerySimpleCounter({end: 224,duration: 2175});
 
 
-// Partners slider
+// image slider
 
+
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+  
+  slides[index].style.display= "block";
+  dot[index].classList.add("active");
+  
+  index++;
+  
+  setTimeout(changeSlide,4000);
+  
+}
+
+changeSlide();
 
 
